16ffa08a3d93c8237db9058a8e5ef39b1e7f8906
EmilWong <emiloffice@gmail.com>
2017-09-26 17:12:45 +0800

    halo everybody

---
+++ resources/views/presskit.blade.php
@@ -0,0 +1,296 @@
+
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no, minimal-ui" name="viewport">
+
+    <link rel="stylesheet" href="/css/googlefonts.css" type="text/css" />
+    <link rel="stylesheet" href="/css/tiny.css?v=2017-08-10" type="text/css" />
+
+    <link rel="stylesheet" href="/css/indienova-style.css?v=2017-08-10" type="text/css" />
+
+    <link rel="shortcut icon" href="/images/indienova_thumb.png" />
+
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="/js/ie10-viewport-bug-workaround.js"></script>
+
+    <!-- External JavaScripts
+    ============================================= -->
+    <script type="text/javascript" src="/js/jquery.js"></script>
+    <script type="text/javascript" src="/js/plugins.js?v=2017-08-10"></script>
+
+    <!-- Document Title ============================================= -->
+    <title>拯救世界特别小队 Press Kit</title>
+    <meta name="description" content="寻找黎明 SeekingDawn by Multiverse VR邮箱" />
+    <meta name="keywords" content="寻找黎明 SeekingDawn RPG 游戏 VR" />
+    <meta name="author" content="seekingdawnvr.com" />
+</head>
+<body class="stretched no-transition">
+
+<!-- Document Wrapper ============================================= -->
+<div id="wrapper" class="clearfix">
+
+    <!-- Content ============================================= -->
+    <section id="content">
+        <div class="content-wrap">
+            <div class="container" id="presskit">
+                <div class="row">
+                    <div class="col-md-3 hidden-xs hidden-sm">
+                        <h1>寻找黎明<br /><small>Press Kit</small></h1>
+                        <ul class="no_disc">
+                            <li><a href="#basic">基本信息</a></li>
+                            <li><a href="#intro">游戏介绍</a></li>
+                            <li><a href="#feature">游戏特性</a></li>
+                            <li><a href="#video">游戏视频</a></li>
+                            <li><a href="#image">游戏图片</a></li>
+                            <li><a href="#logoicon">Logo & Icon</a></li>
+                            <li><a href="#award">获奖情况</a></li>
+                            <li><a href="#report">媒体报道</a></li>
+                            <li><a href="#links">相关链接</a></li>
+                            <li><a href="#team">团队成员</a></li>
+                            <li><a href="#contact">联系方式</a></li>
+                            {{--<li><a href="http://steamcommunity.com/sharedfiles/filedetails/?id=727120769" target="_blank">Steam Greenlight</a></li>--}}
+                        </ul>
+                    </div>
+                    <div class="col-xs-12 col-ms-12 col-md-9">
+                        <img src="https://indienova.com/farm/gameimg/0/signal-decay_logo_800.jpg" class="img-rounded" width="100%">
+                        <div class="row topmargin-sm">
+                            <a name="basic"></a>
+                            <div class="col-xs-12 col-ms-12 col-sm-4 col-md-3">
+                                <h3>基本信息</h3>
+                                <ul class="no_disc vertical_space">
+                                    <li><strong>官方网站：</strong><br /><a href="http://seekingdawnvr.com" target="_blank">官方网站</a></li>
+                                    <li><strong>开发团队：</strong><br /><a href="https://www.multiverseinc.com">Multiverse Entertainment</a></li>
+                                    <li><strong>当前版本：</strong><br />Alpha</li>
+                                    <li><strong>主要类型：</strong><br />沙盒生存</li>
+                                    <li><strong>次要类型：</strong><br />科幻RPG</li>
+                                    <li><strong>支持平台：</strong><br />
+                                        PSVR/HTC VIVE/Oculus                                                                            /                                         Mac                                                                             /                                         PS4                                                                        </li>
+                                    <li><strong>发行日期：</strong><br />尚未确定</li>
+                                    <li><strong>完成进度：</strong><br />60%</li>
+                                </ul>
+                            </div>
+                            <div class="col-xs-12 col-ms-12 col-sm-8 col-md-9">
+                                <a name="intro"></a>
+                                <article class="game-intro">
+                                    <h3>详细介绍</h3>
+                                    <p><em>寻找黎明 (Seeking Dawn) 是一个以未来世界为背景的 沙盒生存类的科幻VR游戏。在一个异星球上，利用仅有的资源进行艰难的求生之旅，通过在星球上的一系列步骤，彻底揭开星球的面纱。</p>
+                                    <p>
+                                        这是一款关于考验玩家如何生存的游戏。在游戏中，玩家将要从最开始的一穷二白到逐渐建设有起色。在宝贵的初期，每个行动都要仔细斟酌审核，完整的利用好在手上的每一个资源去生存。同时游戏还具有多人模式，与朋友战友一同建设基地，共同为生存一起奋斗。只不过人越多，需要消耗的资源也将越多。如何利用仅限的资源在这个穷困的环境生存，才是最考验玩家的地方。
+                                    </p>                                                                    <a name="feature"></a>
+                                    <hr>
+                                    <h3>游戏特性</h3>
+                                    <ul>
+                                        <li>丰富的武器定义未来战争。每个武器都有独特的优缺点，利用好武器切换系统灵活的扬长避短。</li>
+                                        <li>资源收集和建筑系统，精心收集全面资源升级建筑，让建筑来提高自己的生存度。</li>
+                                        <li>多人协作生存，带上朋友一同进行生存。在困惑与挣扎中并肩前行，一同缔造最完美的家园。</li>
+                                        <li>自由的沙盘模式，是生存还是战斗，是建设家园还是一路冒险，多样道路自由抉择，不同的选择缔造不一样的结局。</li>
+                                    </ul>
+                                </article>
+                            </div>
+                            <div class="clearfix"></div>
+
+                            <a name="video"></a>
+                            <hr>
+                            <h3>游戏视频</h3>
+                            <div class="embed-responsive embed-responsive-16by9 topmargin-sm">
+                                <iframe src="https://player.youku.com/embed/XMTY0OTQ0NTM1Mg==" class="embed-responsive-item" frameborder="0"></iframe>
+                            </div>
+                            <small>
+                                <a href="http://v.youku.com/v_show/id_XMTY0OTQ0NTM1Mg==.html" target="_blank">http://v.youku.com/v_show/id_XMTY0OTQ0NTM1Mg==.html</a>
+                            </small>
+
+                            <a name="image"></a>
+                            <hr>
+                            <h3>游戏图片</h3>
+                            <div class="row clearfix" data-lightbox="gallery">
+                                <div class="col-xs-12 col-ms-6 col-sm-4 bottommargin-sm">
+                                    <a href="https://static.indienova.com/farm/gameimg/2017/04/48-1492006222.png" data-lightbox="gallery-item">
+                                        <img src="https://static.indienova.com/farm/gameimg/2017/04/48-1492006222_640.jpg">
+                                    </a>
+
+                                </div>
+                                <div class="col-xs-12 col-ms-6 col-sm-4 bottommargin-sm">
+                                    <a href="https://static.indienova.com/farm/gameimg/2017/04/48-1492006223.png" data-lightbox="gallery-item">
+                                        <img src="https://static.indienova.com/farm/gameimg/2017/04/48-1492006223_640.jpg">
+                                    </a>
+
+                                </div>
+                                <div class="col-xs-12 col-ms-6 col-sm-4 bottommargin-sm">
+                                    <a href="https://static.indienova.com/farm/gameimg/2017/04/48-1492006224.png" data-lightbox="gallery-item">
+                                        <img src="https://static.indienova.com/farm/gameimg/2017/04/48-1492006224_640.jpg">
+                                    </a>
+
+                                </div>
+                                <div class="col-xs-12 col-ms-6 col-sm-4 bottommargin-sm">
+                                    <a href="https://static.indienova.com/farm/gameimg/2017/04/48-1492006257.png" data-lightbox="gallery-item">
+                                        <img src="https://static.indienova.com/farm/gameimg/2017/04/48-1492006257_640.jpg">
+                                    </a>
+
+                                </div>
+                                <div class="col-xs-12 col-ms-6 col-sm-4 bottommargin-sm">
+                                    <a href="https://static.indienova.com/farm/gameimg/2017/04/48-1492006258.png" data-lightbox="gallery-item">
+                                        <img src="https://static.indienova.com/farm/gameimg/2017/04/48-1492006258_640.jpg">
+                                    </a>
+
+                                </div>
+                                <div class="col-xs-12 col-ms-6 col-sm-4 bottommargin-sm">
+                                    <a href="https://static.indienova.com/farm/gameimg/2017/04/48-1492006259.png" data-lightbox="gallery-item">
+                                        <img src="https://static.indienova.com/farm/gameimg/2017/04/48-1492006259_640.jpg">
+                                    </a>
+
+                                </div>
+                                <div class="col-xs-12 col-ms-6 col-sm-4 bottommargin-sm">
+                                    <a href="https://static.indienova.com/farm/gameimg/2017/04/48-1492006273.png" data-lightbox="gallery-item">
+                                        <img src="https://static.indienova.com/farm/gameimg/2017/04/48-1492006273_640.jpg">
+                                    </a>
+
+                                </div>
+                            </div>
+
+                            <a name="logoicon"></a>
+                            <hr>
+                            <h3>游戏 Logo & Icon</h3>
+                            <div class="clearfix" data-lightbox="gallery">
+                                <div class="col-xs-12 col-ms-6 col-sm-6 col-md-6 col-lg-4 bottommargin-sm">
+                                    <a href="https://static.indienova.com/farm/gameimg/0/48-1458695332.png" data-lightbox="gallery-item">
+                                        <img src="https://static.indienova.com/farm/gameimg/0/48-1458695332.png" class="img-responsive">
+                                    </a>
+                                </div>
+                                <div class="col-xs-12 col-ms-6 col-sm-6 col-md-6 col-lg-4 bottommargin-sm">
+                                    <a href="https://static.indienova.com/farm/gameimg/0/48-1469312447.png" data-lightbox="gallery-item">
+                                        <img src="https://static.indienova.com/farm/gameimg/0/48-1469312447.png" class="img-responsive">
+                                    </a>
+                                </div>
+                            </div>
+
+                            <a name="award"></a>
+                            <hr>
+                            <h3>获奖情况</h3>
+                            <ul>
+                                <li>
+                                    <a href="#" target="_blank">
+                                        游戏陀螺年度金陀螺奖最受期待VR游戏（2017）<span class="text-date">（2015）</span>                                            </a>
+                                </li>
+
+                                <li>
+                                    <a href="#" target="_blank">
+                                        腾讯GAD&HTC VIVE 最佳画面奖（2017）<span class="text-date">（2016）</span>                                            </a>
+                                </li>
+
+
+
+                            </ul>
+
+                            <a name="report"></a>
+                            <hr>
+                            <h3>媒体报道</h3>
+                            <ul>
+                                <li>
+                                    <strong>VrFocus：</strong><a href="https://www.vrfocus.com/2017/06/one-of-the-largest-vr-action-survival-games-seeking-dawn/" target="_blank">One Of The Largest VR Action Survival Games: Seeking Dawn</a>
+                                </li>
+
+                                <li>
+                                    <strong>UploadVR：</strong><a href="https://uploadvr.com/e3-2017-hands-sci-fi-shooter-seeking-dawn/" target="_blank">E3 2017: Hands-On With Ambitious Sci-Fi Shooter Seeking Dawn</a>
+                                </li>
+
+                                <li>
+                                    <strong>PlayStation：</strong><a href="http://www.playstationlifestyle.net/2017/06/20/seeking-dawn-vr-preview-e3-2017-rough-around-the-edges/" target="_blank">E3 2017 – Seeking Dawn Preview – Rough Around the Edges (PSVR)</a>
+                                </li>
+
+                            </ul>
+
+
+
+                            <a name="links"></a>
+                            <hr>
+                            <h3>相关链接</h3>
+                            <ul>
+                                <li><a href="http://steamcommunity.com/sharedfiles/filedetails/?id=727120769" target="_blank">Steam 绿光</a></li>
+                            </ul>
+
+                            <hr>
+                            <div class="row clearfix">
+                                <a name="contact"></a>
+                                <div class="col-ms-12 col-sm-6">
+                                    <h3>联系方式</h3>
+                                    <ul class="no_disc vertical_space">
+                                        <li>
+                                            <strong>深圳摩登世纪科技</strong><br />
+                                            <a href="Chenjun.li@multiverseinc.com" target="_blank">邮件联系</a>
+
+                                        </li>
+                                    </ul>
+                                </div>
+                                <a name="team"></a>
+                                <div class="col-ms-12 col-sm-6">
+                                    <h3>团队成员</h3>
+                                    <ul class="no_disc vertical_space">
+                                        <li>
+                                            <strong>
+                                                <a href="https://www.multiverseinc.com" target="_blank">范威洋</a>
+
+                                            </strong>CEO
+                                        </li>
+                                        <li>
+                                            <strong>
+                                                <a href="https://www.multiverseinc.com" target="_blank">陈琛文</a>
+
+                                            </strong>ART
+                                        </li>
+                                        <li>
+                                            <strong>
+                                                <a href="https://www.multiverseinc.com" target="_blank">Jiwon</a>
+
+                                            </strong>ART
+                                        </li>
+                                        <li>
+                                            <strong>
+                                                <a href="https://www.multiverseinc.com" target="_blank">程功</a>
+
+                                            </strong>制作人
+                                        </li>
+                                        <li>
+                                            <strong>
+                                                <a href="https://www.multiverseinc.com" target="_blank">Yohan博士</a>
+
+                                            </strong>工程师
+                                        </li>
+                                        <li>
+                                            <strong>
+                                                <a href="https://www.multiverseinc.com" target="_blank">Alex</a>
+
+                                            </strong>技术领队
+                                        </li>
+                                    </ul>
+
+                                </div>
+                            </div>
+
+                            <hr>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="hidden">
+                <script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1000033182'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s22.cnzz.com/z_stat.php%3Fid%3D1000033182%26show%3Dpic1' type='text/javascript'%3E%3C/script%3E"));</script>
+            </div>            </div>
+    </section><!-- #content end -->
+
+</div><!-- #wrapper end -->
+
+<!-- Go To Top ============================================= -->
+<div id="gotoTop" class="icon-angle-up"></div>
+
+<!-- Footer Scripts ============================================= -->
+<script type="text/javascript" src="/assets/js/functions.js"></script>
+<script type="text/javascript" src="/assets/js/main1608.js"></script>
+</body>
+</html>
